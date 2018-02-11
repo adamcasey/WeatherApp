58cc6c6b0af1336a420c6ef8e9047f5157b3ea25
adam casey <adam-casey@live.com>
2018-02-10 17:55:12 -0700

    Update GradesAwk.sh
    Now prints with the correct format

--- GradesAwk.sh
+++ GradesAwk.sh
@@ -5,6 +5,6 @@ if [ -z "$1" ]; then
 
 else
 
-awk '{sum=0; n=0; for(i=4;i<=NF;i++) {sum+=$i; ++n} print sum/n, "[" $1 "] " $3 "," $2}' $1 | sort -k3 -k1
-
+awk '{sum=0; n=0; average=0; for(i=4;i<=NF;i++) {sum+=$i; ++n} {average=int(sum/n)}
+ print average, "[" $1 "] " $3 " ," $2}' $1 | sort -k3 -k1
 fi
