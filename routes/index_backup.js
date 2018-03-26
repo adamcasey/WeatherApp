1461b00883529c9d6bf2c9abc01a3c8357f06e69
adamcasey <adam-casey@live.com>
2018-03-26 13:31:11 -0600

    New User Registration Insert into DB
    Everything is connected to the expresscc db and it registers a new user with a username, useremail and userpassword! Need to figure out a way to assign a new userID but this is a big step

--- routes/index_backup.js
+++ routes/index_backup.js
@@ -1,3 +1,5 @@
+/* This file works before going into the /register stuff */
+
 var express = require('express');
 var router = express.Router();
 
@@ -7,25 +9,25 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/register', function(req, res, next) {
-	const Username = req.body.username;
+/*	const Username = req.body.username;
 	const Useremail = req.body.email;
-	const Userpassword = req.body.password;
-	var randomnumber=Math.floor(Math.random()*11)
+	const Userpassword = req.body.password;*/
 
-	console.log(Username);
-	console.log(Useremail);
-	console.log(Userpassword);
+/*	console.log();
+	console.log();
+	console.log();*/
 
 	//This is what actually connects us to the db
-	const db = require('../db.js')
+/*	const db = require('../db.js')
 	//Access databse object and call a query on it
 	//This should actually insert a new user into the db
-	db.query('INSERT INTO users (Username, Useremail, Userpassword) VALUES (?, ?, ?)', [Username, Useremail, Userpassword],
-		function(error, results, fields) {
+	db.query('INSERT INTO users (Username, Useremail, Userpassword')
+		VALUES ('?, ?, ?' [Username, Useremail, Userpassword], function(
+			error, results, fields) {
 			if (error) throw error;
 
-			//res.render('register', { title: 'Registration Complete!' });
-		});
+			res.render('index', { title: 'Registration Complete!' });
+		})*/
 	res.render('index', { title: 'Registration Complete!' });
 });
 module.exports = router;
