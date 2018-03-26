c0d6bc8a4a0579b007f4457c058dfa59edbfd091
adamcasey <adam-casey@live.com>
2018-03-26 13:39:25 -0600

    Random UserID
    Now includes a random number assigned to the UserID but doesn't check for duplicates

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,18 @@ router.post('/register', function(req, res, next) {
 	const Username = req.body.username;
 	const Useremail = req.body.email;
 	const Userpassword = req.body.password;
-	var randomnumber=Math.floor(Math.random()*11)
+	var UserID=Math.floor(Math.random()*11)
 
 	console.log(Username);
 	console.log(Useremail);
 	console.log(Userpassword);
+	console.log(UserID);
 
 	//This is what actually connects us to the db
 	const db = require('../db.js')
 	//Access databse object and call a query on it
 	//This should actually insert a new user into the db
-	db.query('INSERT INTO users (Username, Useremail, Userpassword) VALUES (?, ?, ?)', [Username, Useremail, Userpassword],
+	db.query('INSERT INTO users (UserID, Username, Useremail, Userpassword) VALUES (?, ?, ?, ?)', [UserID, Username, Useremail, Userpassword],
 		function(error, results, fields) {
 			if (error) throw error;
 
