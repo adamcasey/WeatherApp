62692d0f27235720a72e64bb11fd1527bb66cfcd
adamcasey <adam-casey@live.com>
2018-03-26 22:28:01 -0600

    Registration Error DOES NOT insert into DB
    Put the insert into db code inside of an 'else' statement so that it doesn't automatically insert bad data even though a user generates an error'. Everything should be working as far as the DB goes but need to now connect this to our game and to make sure registered user is able to log in.

--- routes/index.js
+++ routes/index.js
@@ -11,13 +11,25 @@ router.get('/', function(req, res, next) {
 router.post('/register', function(req, res, next) {
 
 	req.checkBody('username', 'Username field cannot be empty.').notEmpty();
+	req.checkBody('username', 'Username must be between 4-15 characters long.').len(4,15);
+	req.checkBody('email', 'The email entered is invalid, please try again.').isEmail();
+	req.checkBody('email', 'Email address must be between 4-100 characters long, please try again.').len(4,100);
+	req.checkBody('password', 'Password must be between 8-100 characters long.').len(8,100);
+	req.checkBody("password", "Password must include one lowercase character, one uppercase character, a number, and special character.").matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.* )(?=.*[^a-zA-Z0-9]).{8,}$/, "i");
+	req.checkBody('passwordMatch', 'Password must be between 8-100 characters long.').len(8,100);
+	req.checkBody('passwordMatch', 'Passwords do not match. Please try again.').equals(req.body.password);
+
+
 	const errors = req.validationErrors();
 
 	if (errors) {
 		console.log(`errors: ${JSON.stringify(errors)}`);
 
-		res.render('register', {title: 'Registration Error'});
-	}
+		res.render('register', {
+			title: 'Registration Error',
+			errors: errors
+		});
+	} else {
 
 	const Username = req.body.username;
 	const Useremail = req.body.email;
@@ -38,10 +50,10 @@ router.post('/register', function(req, res, next) {
 	db.query('INSERT INTO users (UserID, Username, Useremail, Userpassword) VALUES (?, ?, ?, ?)', [UserID, Username, Useremail, Userpassword],
 		function(error, results, fields) {
 			if (error) throw error;
-
-			//res.render('register', { title: 'Registration Complete!' });
+			res.render('register', { title: 'Registration Complete!' });
 		});
-	res.render('register', { title: 'Registration Complete!' });
+	//res.render('register', { title: 'Registration Complete!' });
+	}
 });
 module.exports = router;
 
